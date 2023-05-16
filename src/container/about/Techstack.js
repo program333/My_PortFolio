931502f7d64d9122011fb08096bca282ea9dd6aa
Devindra <devindrahirabatti26@gmail.com>
2023-05-16 17:17:07 +0530

    Resume

--- src/container/about/Techstack.js
+++ src/container/about/Techstack.js
@@ -24,10 +24,13 @@ function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
+        <CgCPlusPlus/>
+       {/* <a href="https://getbootstrap.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="50" height="50"/> </a> */}
       </Col>
+      
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+        {/* <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="50" height="50"/></a> */}
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
